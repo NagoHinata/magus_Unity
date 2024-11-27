701a2d6600ea1529fe6c0b927c3169f9877cf515
Teppei032 <157445990+Teppei032@users.noreply.github.com>
2024-11-27 16:14:39 +0900

    up

--- メイガスバトル/Assets/Scripts/TitleOpsyon.cs
+++ メイガスバトル/Assets/Scripts/TitleOpsyon.cs
@@ -3,9 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class TitleOpsyon:MonoBehaviour
 {
+
+    
+
     public void Onclick()
     {
         SceneManager.LoadScene("Option");
+       
     }
     
 }
