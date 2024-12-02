0692958387d559a9a8d1ffadc69656448654bcb0
NagoHinata <oibc35033@ojs.ac.jp>
2024-12-02 09:47:10 +0900

    update

--- メイガスバトル/Assets/Scripts/ChangeSelect.cs
+++ メイガスバトル/Assets/Scripts/ChangeSelect.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeOrganization : MonoBehaviour
+public class ChangeSelect : MonoBehaviour
 {
     public void OnClick() // ターンエンドボタンにつける処理
     {
-        SceneManager.LoadScene("DeckOrganization");
+        SceneManager.LoadScene("Select");
     }
 }
--- メイガスバトル/Assets/Scripts/ChangeTitle.cs
+++ メイガスバトル/Assets/Scripts/ChangeTitle.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeOrganization : MonoBehaviour
+public class ChangeTitle : MonoBehaviour
 {
     public void OnClick() // ターンエンドボタンにつける処理
     {
-        SceneManager.LoadScene("DeckOrganization");
+        SceneManager.LoadScene("Title");
     }
 }
