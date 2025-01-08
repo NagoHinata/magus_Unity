e6afedd1af9ff91d9af7ce1f8f496a74a9c44486
Teppei032 <157445990+Teppei032@users.noreply.github.com>
2025-01-08 09:33:24 +0900

    up

---
+++ メイガスバトル/Assets/Scripts/SEstate.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SEstate : MonoBehaviour
+{
+    private AudioSource button_AudioSource;
+    [SerializeField] AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        button_AudioSource = GetComponent<AudioSource>();
+
+        //画面遷移してもオブジェクトが壊れないようにする
+        DontDestroyOnLoad(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ButtonClick()
+    {
+        button_AudioSource.PlayOneShot(button_AudioSource.clip);
+
+        //音楽を鳴らす
+        audioSource.PlayOneShot(audioSource.clip);
+
+        //鳴り始めたことを表示
+        Debug.Log("開始");
+        
+        yield return new WaitWhile(() => audioSource.isPlaying);
+
+    }
+
+
+}
